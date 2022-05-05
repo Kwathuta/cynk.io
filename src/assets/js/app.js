4b626283d9be143887f46a1daf97bb3b8f7174d6
Kelvin Gitahi <kgitahi@zohomail.com>
2022-05-05 12:13:19 +0300

    adding changes to homepage

---
+++ src/assets/js/app.js
@@ -0,0 +1,7 @@
+const menuToggle = document.querySelector('.toggle')
+const videoWrapper = document.querySelector('.video-wrapper')
+
+// menuToggle.addEventListener('click', () => {
+//   menuToggle.classList.toggle('active')
+//   videoWrapper.classList.toggle('active')
+// })
